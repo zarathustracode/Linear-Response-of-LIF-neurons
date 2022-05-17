ed0ebe985b4482656984a689eae2b52d62fcdadb
Nebojsa Gasparovic <nebojsa.gasparovic@bcf.uni-freiburg.de>
2022-05-17 16:59:59 +0200

    Adding basic GET method and basic data validation

--- lifAPI/SynapticCurrent.py
+++ lifAPI/SynapticCurrent.py
@@ -1,11 +1,11 @@
 
 from typing import List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Current(BaseModel):
-    mean: float
-    variance: float
+    mean: float = Field(gt=-10, le=10, default=1)
+    variance: float = Field(ge=1, le=5, default=1)
 
 
 class NeuronalResponse(BaseModel):
--- lifAPI/main.py
+++ lifAPI/main.py
@@ -1,5 +1,5 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
 from .SynapticCurrent import Current, NeuronalResponse
 from .ioLIF import *
 
@@ -26,5 +26,17 @@ async def membrane_potential(data: Current):
             'probability flux': list(J0)
             }
 
+@app.get("/get_stationary_diststribution/{tag}/")
+async def get_membrane_potential(
+        mean_val: float = Query(default=0., gt=-10, le=10),
+        variance_val:float = Query(default=1., ge=1, le=5),
+        tag: str = Path(default='calculate')):
 
+    V,P0,J0,r0 = LIF0(tau=tau_mem,mu=mean_val,sig=variance_val,Vth=V_th, Vresting=V_resting,Vre=V_reset,tau_ref=t_ref)
 
+    return {
+            'membrane potential values': list(V),
+            'probability distribution': list(P0),
+            'probability flux': list(J0),
+            'tag': tag
+            }
