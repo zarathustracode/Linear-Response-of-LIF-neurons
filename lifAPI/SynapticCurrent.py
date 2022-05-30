c378c5398a80e7148ad2878db6bdfb8db1a219fd
Nebojsa Gasparovic <nebojsa.gasparovic@bcf.uni-freiburg.de>
2022-05-30 14:49:52 +0200

    Allow all orgins and CORS

--- lifAPI/SynapticCurrent.py
+++ lifAPI/SynapticCurrent.py
@@ -1,4 +1,5 @@
 
+import string
 from typing import List
 
 from pydantic import BaseModel, Field
@@ -6,9 +7,3 @@ from pydantic import BaseModel, Field
 class Current(BaseModel):
     mean: float = Field(gt=-10, le=10, default=1)
     variance: float = Field(ge=1, le=5, default=1)
-
-
-class NeuronalResponse(BaseModel):
-    grid: List[float]
-    probabilities: List[float]
-    fluxes: List[float]
--- lifAPI/main.py
+++ lifAPI/main.py
@@ -1,20 +1,34 @@
 
-from fastapi import FastAPI, Path, Query
-from .SynapticCurrent import Current, NeuronalResponse
+from fastapi import FastAPI, Path, Query, Request
+from .SynapticCurrent import Current
 from .ioLIF import *
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI(
         title="Leaky integrate-and-fire neurons API",
         description="API endpoint for Python/C++ program")
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get('/')
-async def index():
+async def index(request: Request):
+    print(request.headers)
     return {'message': 'This is LIF API'}
 
 
 @app.post('/neuron')
 async def membrane_potential(data: Current):
-    data = data.dict()
+    #data = data.dict()
+    data = jsonable_encoder(data)
     mean = data['mean']
     variance = data['variance']
 
@@ -35,8 +49,8 @@ async def get_membrane_potential(
     V,P0,J0,r0 = LIF0(tau=tau_mem,mu=mean_val,sig=variance_val,Vth=V_th, Vresting=V_resting,Vre=V_reset,tau_ref=t_ref)
 
     return {
-            'membrane potential values': list(V),
-            'probability distribution': list(P0),
-            'probability flux': list(J0),
+            'grid': list(V),
+            'probabilities': list(P0),
+            'fluxes': list(J0),
             'tag': tag
             }
